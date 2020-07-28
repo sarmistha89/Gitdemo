0bf6391197f73a452049feae6fb6a282de2d3451
Sarmistha <nayaksarmistha@gmail.com>
2020-07-28 22:51:28 +0530

    change by 2nd user

--- test_e2e.py
+++ test_e2e.py
@@ -37,6 +37,8 @@ class Testoperationone(BaseClass):
 
         assert "Success" in alertmessage
         self.driver.get_screenshot_as_file("screen.png")
+        
+        print("changed by gitusa")
     
 
 
